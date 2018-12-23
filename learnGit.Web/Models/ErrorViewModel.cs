516d963d02314ffd6659938a56dc005d306ba19b
yiyungent <yiyungent@126.com>
2018-12-23 14:23:44 +0800

    add a project, .travis.yml

---
+++ learnGit.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace learnGit.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
